e0245efaf1359ce268bbbfa7e267e123209cb16e
ghost <noreply@localhost>
2022-05-06 22:45:46 +0300

    add image cache support

--- public/image.php
+++ public/image.php
@@ -4,8 +4,16 @@ require_once('../library/icon.php');
 
 if (isset($_GET['hash'])) {
 
-  $icon = new Icon();
-
   header("Content-Type: image/jpeg");
-  echo $icon->generateImageResource(md5($_GET['hash']), 60, 60, false);
+
+  $filename = dirname(__FILE__) . '/../cache/' . $_GET['hash'] . '.jpeg';
+
+  if (!file_exists($filename)) {
+
+    $icon = new Icon();
+
+    file_put_contents($filename, $icon->generateImageResource(md5($_GET['hash']), 60, 60, false));
+  }
+
+  echo file_get_contents($filename);
 }
